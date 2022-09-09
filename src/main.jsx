b49bfcf04b622a172060b299cfb864baefd1b6b2
shoyan <yamasaki0406@gmail.com>
2022-09-09 21:52:38 +0900

    3 files modified, 3 added

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,31 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./main.css";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import './main.css';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
+import Invoice from './routes/invoice';
 
-let rootElement = document.getElementById("root");
+let rootElement = document.getElementById('root');
 ReactDOM.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>
-, rootElement);
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
---
+++ src/routes/invoice.jsx
@@ -0,0 +1,25 @@
+import { useParams } from "react-router-dom";
+import { getInvoice } from "../data";
+
+export default function Invoice() {
+  let params = useParams();
+  let invoice = getInvoice(parseInt(params.invoiceId, 10));
+  
+  if(invoice) {
+    return (
+      <main style={{ padding: "1rem" }}>
+        <h2>Total Due: {invoice.amount}</h2>
+        <p>
+          {invoice.name}: {invoice.number}
+        </p>
+        <p>Due Date: {invoice.due}</p>
+      </main>
+    );
+  } else {
+    return (
+      <main style={{ padding: '1rem' }}>
+        <p>There's nothing here!</p>
+      </main>
+    )
+  }
+}
